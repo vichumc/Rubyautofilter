c8fd7c5fd3a9556dd96079c83aedce13513264b8
Daneil-k <158211667+Daneil-k@users.noreply.github.com>
2024-03-31 11:23:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,9 @@
 import re
 from os import environ
-from Script import script 
+from Script import script
+from dotenv import load_dotenv
 
+load_dotenv("./dynamic.env", override=True)
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
